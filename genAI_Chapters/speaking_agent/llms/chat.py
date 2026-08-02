bac51d2e6cc1f913f7da9c945797865c0d8962d3
Ghayaz <mohammedghayaz949@gmail.com>
2026-08-02 23:55:54 +0530

    some minor debugs, nothing main to continue i am still designing it

--- genAI_Chapters/speaking_agent/llms/chat.py
+++ genAI_Chapters/speaking_agent/llms/chat.py
@@ -2,14 +2,14 @@ from client import get_genai_client
 from google.genai import types
 from config import MODEL_NAME
 from prompts import SYSTEM_PROMPT
-from tools_schema import TOOLS
+from tools_schema import TOOLS as tools
 def generate_content(history):
     response = get_genai_client().models.generate_content(
                     model=MODEL_NAME,  # Gemini me generate_content ke liye sahi model use karein
                         contents=history,          # OpenAI ke 'messages' ki jagah 'contents' use hota hai
                         config=types.GenerateContentConfig(
                         system_instruction=SYSTEM_PROMPT,    
-                        tools=TOOLS            # Tools ko config ke andar pass kiya jata hai
+                        tools=tools            # Tools ko config ke andar pass kiya jata hai
                         )
                     )
     return response
--- genAI_Chapters/speaking_agent/tools_unit/command.py
+++ genAI_Chapters/speaking_agent/tools_unit/command.py
@@ -4,7 +4,7 @@ def execute_command(command: str, timeout: int = 120):
     try:
         result = subprocess.run(
             command,
-            shell=True,# ye chiz python ku bolti ki system ke native command shell me function chalao. naki kisi lab ya powershell me
+            shell=True,# ye chiz python ku bolti ki system ke native command shell me function chalao.
             capture_output=True, # It redirects and intercepts both the standard output (stdout) and standard error (stderr) of the running process,
             # stopping it from printing to the screen.
             text=True, #  It tells Python to automatically decode the incoming raw bytes(pc ki basha) from the operating system into a clean Python string 
