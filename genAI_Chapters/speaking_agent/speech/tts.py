48e39432fa769d0af2d5d295b39993f7a9a9e1da
Ghayaz <mohammedghayaz949@gmail.com>
2026-08-10 02:11:30 +0530

    audio files are in boundaries

--- genAI_Chapters/speaking_agent/speech/tts.py
+++ genAI_Chapters/speaking_agent/speech/tts.py
@@ -1,18 +1,14 @@
 from llms.client import client
 from config import TTS_MODEL, VOICE_NAME
 import traceback
-def generate_tts(final_content):
-    try: # speech client exception handling
-        print("creating audio through ai response")
-        interaction = client.interactions.create(
-        model=TTS_MODEL,
-        input=f"Speak naturally and conversationally: {final_content}", # defining style and input in input feild as google specifies
-        response_format={"type": "audio"}, # response type declare
-        generation_config={"speech_config": [{"voice": VOICE_NAME}]} # speaker type 
-        ) 
-        print("ai audio created")
-    except Exception as e:
-        print(f"TTS error: {e}")
-        traceback.print_exc()
-    return interaction            
-                
+import base64
+def generate_tts(final_content)-> bytes:
+    print("creating audio through ai response")
+    interaction = client.interactions.create(
+    model=TTS_MODEL,
+    input=f"Speak naturally and conversationally: {final_content}", # defining style and input in input feild as google specifies
+    response_format={"type": "audio"}, # response type declare
+    generation_config={"speech_config": [{"voice": VOICE_NAME}]} # speaker type 
+    )
+    print("ai audio created")
+    return base64.b64decode(interaction.output_audio.data)
--- genAI_Chapters/speaking_agent/workflow/chat_loop.py
+++ genAI_Chapters/speaking_agent/workflow/chat_loop.py
@@ -1,26 +1,29 @@
-import speech 
+from speech import tts, audio,microphone,stt
 from llms import history,chat
 from workflow import tool_handler
-import base64 
-import traceback
+import traceback, types
+history_log: list[types.Content] = []
 try :
     while True:
-        user_query = speech.stt.speech_to_text(audio= speech.microphone.Recognizer() )
+        user_query = stt.speech_to_text(audio= microphone.Recognizer() )
         print(f"User: ", user_query)
         if user_query.lower().strip() in ["exit", "band hojao","bye"]:
             print("Irshard V2: Acha waqt bacha raha toh phir milinge")
             break
-        history_log = history.append_user_query( user_audio_to_text= user_query )
+        history_log = history.append_user_query( history=history_log,user_audio_to_text= user_query )
         
         response = chat.generate_content(history=history_log)
         final_content = tool_handler.function_handler(response=response , history_log=history_log)
-        interaction = speech.tts.generate_tts(final_content=final_content)
+        audio_data = tts.generate_tts(final_content)
         print("🗣️LLM:", final_content)
         
         print("playing ai audio")
-        wav_file = speech.audio.wave_file('out.wav', base64.b64decode(interaction.output_audio.data))
+        wav_file = audio.wave_file(
+            "out.wav",
+            audio_data
+        )
         try:
-            speech.audio.play_audio("out.wav")
+            audio.play_audio("out.wav")
         except RuntimeError as e:
             print(e)
         history_log = history.append_assistant(response=response)
